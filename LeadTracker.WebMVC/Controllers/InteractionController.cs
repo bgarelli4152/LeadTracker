5e401cd07620cbc06ea29494e778cbb4e1a8a6ac
bgarelli4152 <63520554+bgarelli4152@users.noreply.github.com>
2020-12-17 18:10:24 -0500

    interaction bug fixed

--- LeadTracker.WebMVC/Controllers/InteractionController.cs
+++ LeadTracker.WebMVC/Controllers/InteractionController.cs
@@ -32,12 +32,76 @@ namespace LeadTracker.WebMVC.Controllers
 
             return RedirectToAction("Index");
         }
+        public ActionResult Edit(int id)
+
+        {
+
+            var service = new InteractionService();
+            var detail = service.GetInteractionById(id);
+            var model =
+                new InteractionEdit
+                {
+                    InteractionID = detail.InteractionID,
+                    TypeOfContact = detail.TypeOfContact,
+                    Description = detail.Description,
+                    ModifiedUtc = DateTimeOffset.Now
+                };
+            return View(model);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, InteractionEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            if (model.InteractionID != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+
+            var service = new InteractionService();
+
+            if (service.UpdateInteraction(model))
+            {
+                TempData["SaveResult"] = "Interaction Details Successfully Updated";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Failed to Update Interaction");
+            return View(model);
+        }
 
         public ActionResult Details(int id)
         {
             var service = new InteractionService();
             var model = service.GetInteractionById(id);
             return View(model);
+        }
+
+        [ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var service = new InteractionService();
+            var model = service.GetInteractionById(id);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost(int id)
+        {
+            var service = new InteractionService();
+
+            service.DeleteInteraction(id);
+
+            TempData["SaveResult"] = "Your interaction was deleted";
+
+            return RedirectToAction("Index");
         }
     }
 }
